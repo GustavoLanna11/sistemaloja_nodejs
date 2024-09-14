c22365891f747cc4f3207ad6e4b2f13491e31fe8
Gustavo Lanna <99662123+GustavoLanna11@users.noreply.github.com>
2024-09-13 22:01:07 -0300

    Add files via upload

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -9,6 +9,8 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+app.use(express.static('public'))
+
 //rota de clientes
 app.get("/clientes", (req, res) => {
     const clientes =[
@@ -24,14 +26,26 @@ app.get("/clientes", (req, res) => {
 //rota de produtos
 app.get("/produtos", (req,res) => {
     const produtos = [
-        {nome: 'Cereal', cpf: '123456789', endereco: 'Rua Pitanga'},
-        {nome: 'Yasmin', cpf: '123456789', endereco: 'Barra'},
-        {nome: 'Ana', cpf: '123456789', endereco: 'Valo Grande'},
-        {nome: 'Isabely', cpf: '123456789', endereco: 'Rua Pitanga'}];
+        {nome: 'Teclado Mecânico', preco: '300', categoria: 'Periféricos'},
+        {nome: 'Mouse', preco: '200', categoria: 'Periféricos'},
+        {nome: 'Computador de mesa', preco: '5000', categoria: 'Computador'},
+        {nome: 'Notebook', preco: '3000', categoria: 'Notebook'}];
         res.render("produtos", {
             produtos: produtos
         })
 })
+
+app.get("/pedidos", (req,res) => {
+    const pedidos = [
+        {numeroPed: '1', valor: '100'},
+        {numeroPed: '2', valor: '200'},
+        {numeroPed: '3', valor: '300'},
+        {numeroPed: '4', valor: '400'}];
+        res.render("pedidos", {
+            pedidos:pedidos
+        })
+})
+
 
 const port = 8080
 app.listen(8080, function (error){
